5ad2d1fdc53a70c52cefc40e4f7e6fc9d037fe9d
friendly-helper <t.i.g.g.r.i.s.1@gmail.com>
2025-11-11 19:25:42 +0100

    Adding css to login.php
    Basic Dark CSS

--- login.php
+++ login.php
@@ -8,6 +8,7 @@ $isLoggedIn = isset($_SESSION['username']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
